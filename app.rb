27eb67e49e5f81bbe77802de14801e0e43ddc1f0
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-15 21:10:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,8 +3,16 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   configure do
-    enable :sessions 
+    enable :sessions
     set :session_secret, "supersecretpa$$word"
   end
-  
+
+  get '/' do
+
+  end
+
+  post '/checkout' do
+    
+  end
+
 end
